c89638505f80719f80fe5908db32e140faa76f7b
BerserkerValle <fsamayoav@miumg.edu.gt>
2024-09-24 09:59:08 -0800

    first commit

---
+++ app/routers/router.js
@@ -0,0 +1,21 @@
+
+let express = require('express');
+let router = express.Router();
+
+ //constasntes de rutas 
+const customers = require('../controllers/controller.js');
+const empleados = require('../controllers/empleado.js')
+const departamentos = require('../controllers/departamento.js');
+
+router.post('/api/customers/create', customers.create);
+router.get('/api/customers/all', customers.retrieveAllCustomers);
+router.get('/api/customers/onebyid/:id', customers.getCustomerById);
+router.get('/api/customers/filteringbyage', customers.filteringByAge);
+router.get('/api/customers/pagination', customers.pagination);
+router.get('/api/customers/pagefiltersort', customers.pagingfilteringsorting);
+router.put('/api/customers/update/:id', customers.updateById);
+router.delete('/api/customers/delete/:id', customers.deleteById);
+
+
+module.exports = router;
+
